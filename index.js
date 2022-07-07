e46ea5476f830a6425f47c87cb8bcd57ebcb9713
fusion407 <fusion407@gmail.com>
2022-07-06 22:39:09 -0500

    commit tree cfc0af62b84c185e169f9463a6e46c8ab4cd89d5

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    // append cat to end of array
+    cats.push(name);
+    return cats;
+}
+function destructivelyPrependCat(name) {
+    // prepend cat to beginning of array
+    cats.unshift(name);
+    return cats;
+}
+function destructivelyRemoveLastCat() {
+    // remove last cat from cats array
+    cats.pop();
+    return cats;
+}
+function destructivelyRemoveFirstCat() {
+    // removes first cat from the cats array
+    cats.shift();
+    return cats;
+}
+function appendCat(name) {
+    // append cat to cats array and return new array
+    const copyCats = cats.slice();
+    copyCats.push(name)
+    return copyCats;
+}
+function prependCat(name) {
+    // prepend cat to cats array and return new array
+    const copyCats = cats.slice();
+    copyCats.unshift(name)
+    return copyCats;
+}
+function removeLastCat() {
+    // remove last cat in cats array and returns new array
+    const copyCats = cats.slice();
+    copyCats.pop();
+    return copyCats;
+}
+function removeFirstCat() {
+    // removes first cat from cats array and returns new array
+    const copyCats = cats.slice();
+    copyCats.shift();
+    return copyCats;
+}
